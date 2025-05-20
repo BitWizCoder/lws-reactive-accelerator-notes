12cfea56b1e8057075a4ca5f0dbfc3f39e957076
Muhammad Noman <mhmdnoman01@gmail.com>
2025-05-20 22:09:26 +0600

    oragnized files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import WritingJSX from "./learning/01-describing-the-ui/03-Writing-JSX/03-Writing-JSX";
+import WritingJSX from "./learning/01-describing-the-ui/03-writing-jsx/03-WritingJSX";
 
 export default function App() {
   return (
